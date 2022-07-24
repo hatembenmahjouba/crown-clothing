3eb29bb80bfd76eb55143909a73d605c90192e7a
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-07-24 17:13:09 +0200

    update our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/hats' element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
