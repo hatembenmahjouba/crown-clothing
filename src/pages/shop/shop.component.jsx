0d3b1e39149b20b40bb8a0a23a40a66e3ce196ab
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-07-28 23:58:49 +0200

    pull and convert shop data in our application and store it into our shop reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = () => (
-  <div className='shop-page'>
-    <Routes>
-      <Route path='/' element={<CollectionsOverview />} />
-      <Route path='/:collectionId' element={<CollectionPage />} />
-    </Routes>
-  </div>
-);
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
-export default ShopPage;
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async (snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+    });
+  }
+
+  render() {
+    return (
+      <div className='shop-page'>
+        <Routes>
+          <Route path='/' element={<CollectionsOverview />} />
+          <Route path='/:collectionId' element={<CollectionPage />} />
+        </Routes>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionsMap) =>
+    dispatch(updateCollections(collectionsMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
