edac71133118002ffb913518587d478b623fc7f1
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-07-28 16:30:11 +0200

    converting directory component over to styled-components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -4,14 +4,14 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 
-import './directory.styles.scss';
+import { DirectoryMenuContainer } from './directory.styles.jsx';
 
 const Directory = ({ sections }) => (
-  <div className='directory-menu'>
+  <DirectoryMenuContainer>
     {sections.map(({ id, ...otherSectionProps }) => (
       <MenuItem key={id} {...otherSectionProps} />
     ))}
-  </div>
+  </DirectoryMenuContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
