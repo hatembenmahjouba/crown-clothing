66e8d7a09c4c70e0c025417c2329a7cb34103c15
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-07-28 16:26:32 +0200

    converting collection-overview component over to styled-components

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component';
+
+import {
+  CollectionPreviewContainer,
+  TitleContainer,
+  PreviewContainer,
+} from './collection-preview.styles';
+
+const CollectionPreview = ({ title, items }) => (
+  <CollectionPreviewContainer>
+    <TitleContainer>{title.toUpperCase()}</TitleContainer>
+    <PreviewContainer>
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </PreviewContainer>
+  </CollectionPreviewContainer>
+);
+
+export default CollectionPreview;
