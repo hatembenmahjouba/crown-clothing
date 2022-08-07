adef7c082d5c35854a34dd25942c4675ab9be8a6
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-08-07 15:25:09 +0200

    !!

--- server.js
+++ server.js
@@ -17,10 +17,9 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
+  app.get('/*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  );
 }
 
 app.listen(port, (error) => {
