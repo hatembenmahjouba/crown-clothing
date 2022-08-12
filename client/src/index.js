838f46921a33ae39b269b80abc3ec43d4176f579
hatembenmahjouba / inter-maghreb <ben.mahjouba.hatem@gmail.com>
2022-08-12 16:50:30 +0200

    updating our code to be pwa compliant, added serviceworker, https, updated manifest

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -26,3 +27,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register();
